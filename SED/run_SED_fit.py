66514033f3c85d2d61aa4485b72b1ab82fa0c9ae
tglauch <theo.glauch@tum.de>
2018-07-13 05:00:38 -0500

    SED updates

--- SED/run_SED_fit.py
+++ SED/run_SED_fit.py
@@ -8,6 +8,22 @@ import yaml
 import pickle
 import pyfits as fits
 
+   
+def setup_data_files(folder):
+    files = os.listdir(folder)
+    if 'events.txt' not in files:
+        ph_files = [os.path.join(folder,f) for f in files if 'PH' in f]
+        with open(os.path.join(folder,'events.txt'), 'w+') as ofile:
+            ofile.write("\n".join(ph_files))
+    if not 'spacecraft.fits' in files:
+        sc_files = [f for f in files if 'SC' in f]
+        if len(sc_files)==1:
+            os.rename(os.path.join(folder,sc_files[0]),
+                      os.path.join(folder,'spacecraft.fits'))
+        else:
+            raise Exception('No Spacecraft File Available!')
+    return
+ 
 def get_time_window(folder):
     files = os.listdir(folder)
     files = [f for f in files if 'PH' in f]
@@ -40,7 +56,10 @@ def parseArguments():
         "--emin",
         help="The minimum energy for the analysis",
         type=float, default=100)
-
+    parser.add_argument(
+        "--emax",
+        help="The minimum energy for the analysis",
+        type=float, default=100000)
     parser.add_argument(
         "--target_src",
         help="The target source for the analysis",
@@ -49,7 +68,7 @@ def parseArguments():
     parser.add_argument(
         "--retries",
         help="Number of retries if fit does not converge",
-        type=int, default=5)
+        type=int, default=20)
 
     parser.add_argument(
         "--free_sources",
@@ -62,8 +81,18 @@ def parseArguments():
         action='store_true', default=False)
 
     parser.add_argument(
+        "--use_3FGL",
+        help="Decide of whether or not to use 3FGL sources",
+        action='store_true', default=False)
+
+    parser.add_argument(
         "--free_norm",
         help="Only free the normalization of the target",
+        action='store_true', default=False)
+
+    parser.add_argument(
+        "--no_sed",
+        help="Only run llh and bowtie, no sed points",
         action='store_true', default=False)
 
     parser.add_argument(
@@ -94,6 +123,7 @@ print("Running with args \n")
 print(args)
 
 emin = args['emin']
+emax = args['emax']
 if args['trange'] is not None:
      print('Use Self-Defined Time Window')
      tmin = MJD_to_MET(args['trange'][0])
@@ -102,9 +132,11 @@ else:
      print('Use Entire Time Window Available')
      tmin, tmax =  get_time_window(args['data_path'])
 
-basepath = './results/{:.0f}_{:.0f}/{}/'.format(float(MET_to_MJD(tmin)),
+basepath = './results/{}/{:.0f}_{:.0f}/{}_{}/'.format(args['target_src'],
+                                        float(MET_to_MJD(tmin)),
                                         float(MET_to_MJD(tmax)),
-                                        emin)
+                                        int(emin), int(emax))
+setup_data_files(args['data_path'])
 if not os.path.exists(basepath):
     os.makedirs(basepath)
 
@@ -116,14 +148,18 @@ with open('default.yaml', 'r') as stream:
         config = yaml.load(stream)
     except yaml.YAMLError as exc:
         print(exc)
-config['selection']['emin'] = args['emin']
+config['selection']['emin'] = emin
+config['selection']['emax'] = emax
 config['selection']['tmin'] = tmin
 config['selection']['tmax'] = tmax
 config['selection']['target'] = src
 config['data']['evfile'] = os.path.join(args['data_path'], config['data']['evfile'].split('/')[-1]) 
 config['data']['scfile'] = os.path.join(args['data_path'], config['data']['scfile'].split('/')[-1])
+config['model']['catalogs'] = []
 if args['xml_path'] is not None:
     config['model']['catalogs'].append(args['xml_path'])
+if args['use_3FGL']:
+    config['model']['catalogs'].append('3FGL')
 with open(config_path, 'w+') as stream:
     config = yaml.dump(config, stream, default_flow_style=False)
 
@@ -132,7 +168,15 @@ with open(config_path, 'w+') as stream:
 gta = GTAnalysis(os.path.join(basepath, 'config.yaml'),
                  logging={'verbosity': 3})
 gta.setup()
-gta.optimize()
+
+
+if not args['free_radius'] is None:
+    print('Free sources in {} deg radius (with TS>2)'.format(args['free_radius']))
+    gta.free_sources(distance=args['free_radius'], minmax_ts=[2,None], exclude = ['isodiff', 'galdiff'])
+elif not args['free_sources'] is None:
+    for src_key in args['free_sources']:
+        print('Free source {} \n'.format(src_key))
+        gta.free_source(src_key)
 
 #Configure Target Source
 if not args['src_gamma'] is None:
@@ -142,34 +186,30 @@ if not args['src_gamma'] is None:
 elif args['free_norm']:
     free_pars = 'norm'
 else:
+    gta.optimize()
     free_pars = None
-print('Free the target source: {}'.format(src))
+print('Free the target source: {} with pars: {}'.format(src, free_pars))
+gta.free_source(src, pars=None, free=False)
 gta.free_source(src, pars=free_pars, free=True)
-
-# LLH Fit to ROI model
-if not args['free_radius'] is None:
-    print('Free sources in {} deg radius (with TS>2)'.format(args['free_radius']))
-    gta.free_sources(distance=args['free_radius'], minmax_ts=[2,None], exclude = ['isodiff', 'galdiff'])
-elif not args['free_sources'] is None:
-    for src_key in args['free_sources']:
-        print('Free source {} \n'.format(src_key))
-        gta.free_source(src_key)
 
 
 # Free all parameters of isotropic and galactic diffuse components
 if args['free_diff']:
     print('Free Diffuse Components')
-    gta.free_source('galdiff')
-    gta.free_source('isodiff')
-out_dict = gta.fit(retries=args['retries'])
+    gta.free_source('galdiff', pars='norm')
+    gta.free_source('isodiff', pars='norm')
+gta.fit(retries=args['retries'])
+gta.write_roi('llh_emin_{}_emax_{}.npy'.format(emin, emax))
 
-gta.write_roi('llh.npy')
-x = np.load(os.path.join(basepath, 'llh.npy'))[()]
-#index = x['sources'][src]['spectral_pars']['Index']['value']
-#print('Use fitted spectral index (gamma = {}) for SED FIT'.format(index))
+print('Calculate Bowtie for {}'.format(src))
+out_dict = gta.bowtie(src)
+np.save(os.path.join(basepath, 'bowtie_emin_{}_emax_{}.npy'.format(emin, emax)), out_dict)
+print('Start SED Fitting')
+print('Free the target source: {} with pars: {}'.format(src, free_pars))
 
-# Override the energy binning and the assumed power-law index
-# within the bin
-sed = gta.sed(src, outfile='sed.fits',
-              loge_bins=list(np.arange(np.log10(emin), np.log10(1e6), 0.5))) #bin_index = index
+if not  args['no_sed']:
+    sed = gta.sed(src, outfile='sed_emin_{}_emax_{}.fits'.format(emin, emax),
+                  loge_bins=list(np.arange(np.log10(emin), np.log10(emax), 0.5)),
+	          free_pars=free_pars,
+                  free_radius = args['free_radius']) #bin_index = index
 #pickle.dump()
